bb12688a85be72252b9ee9e7d88e920a66a08374
Rishabh Jain <rishabh.jain@walmartlabs.com>
2020-08-05 08:33:24 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import random
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/upload')
 def upload_file1():
-    return render_template('home.html')
+	return render_template('home.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    	n=random.randint(1000,9999)
+    	while(os.path.isdir(str(n)):
+    		n=random.randint(1000,9999)
+		os.chdir(str(n))
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		os.chdir('../')
+		return str(n)
 		
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.run(debug = True)
